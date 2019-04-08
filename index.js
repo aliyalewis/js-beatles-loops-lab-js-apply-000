39ecee80098205295664e439c9c191e4d8873127
Aliya Lewis <aliyalewis5@gmail.com>
2019-04-08 18:36:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function theBeatlesPlay(muscian, instrument) {
   var array = [];
-  for(let i = 0, i < musician.length, i++);
+  for(let i = 0, i>musician.length, i++);
   return `${musican} plays ${instrument}`
 }
